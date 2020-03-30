89f468d16c4dfe45579eb6b1cde9306eb50d2dfa
neronmoon <alistar.neron@gmail.com>
2020-03-30 12:14:03 +0200

    Fix stop blocking with large interval

--- mediatorr/workers/worker.py
+++ mediatorr/workers/worker.py
@@ -1,16 +1,22 @@
 import time
 import threading
+import logging
 
 
 class Worker(threading.Thread):
     alive = True
     interval = 5
+    last_run = 0
 
     def run(self):
         self.on_start()
         while self.alive:
-            self.tick()
-            time.sleep(self.interval)
+            current_time = time.time()
+            if current_time - self.last_run > self.interval:
+                self.tick()
+                self.last_run = current_time
+            time.sleep(1)
+        logging.info("Worker %s stopped" % self.__get_name())
 
     def on_start(self):
         pass
@@ -19,4 +25,8 @@ class Worker(threading.Thread):
         pass
 
     def stop(self):
+        logging.info("Worker %s stop requested" % self.__get_name())
         self.alive = False
+
+    def __get_name(self):
+        return self.__class__.__name__
